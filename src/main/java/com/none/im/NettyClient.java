0ab6af30535f57cf26d83454107596c2d9566394
1158558425 <1610130044@vip.henu.edu.cn>
2019-04-23 23:35:27 +0800

    netty IM

---
+++ src/main/java/com/none/im/NettyClient.java
@@ -0,0 +1,40 @@
+package com.none.im;
+
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @Author: zl
+ * @Date: 2019/4/23 0:54
+ */
+public class NettyClient {
+    public static void main(String[] args) {
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap
+                // 1.指定线程模型
+                .group(workerGroup)
+                // 2.指定 IO 类型为 NIO
+                .channel(NioSocketChannel.class)
+                // 3.IO 处理逻辑
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) {
+                    }
+                });
+        // 4.建立连接
+        bootstrap.connect("localhost", 80).addListener( future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else {
+                System.err.println("连接失败!");
+            }
+
+        });
+    }
+}
---
+++ src/main/java/com/none/im/NettyServer.java
@@ -0,0 +1,31 @@
+package com.none.im;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @Author: zl
+ * @Date: 2019/4/23 0:53
+ */
+public class NettyServer {
+    public static void main(String[] args) {
+        //定义两大
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                    }
+                });
+
+        serverBootstrap.bind("noneserver",8000);
+        System.out.print("启动成功");
+    }
+}
